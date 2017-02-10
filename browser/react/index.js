3bb29094af8000324aac279c91af8c2dfe6c667b
Justin Zhou <justinzhou93@gmail.com>
2017-02-10 14:17:30 -0500

    second commit

--- browser/react/index.js
+++ browser/react/index.js
@@ -9,6 +9,7 @@ import Artist from './components/Artist';
 import Songs from './components/Songs';
 import NewPlaylistContainer from './containers/NewPlaylistContainer';
 import Playlist from './components/Playlist';
+import lyricsContainer from './containers/lyricsContainer';
 
 ReactDOM.render(
   <Router history={hashHistory}>
@@ -22,8 +23,37 @@ ReactDOM.render(
       </Route>
       <Route path="/new-playlist" component={NewPlaylistContainer} />
       <Route path="playlists/:playlistId" component={Playlist} />
+      <Route path="/lyrics" component = {lyricsContainer} />
       <IndexRedirect to='/albums' />
     </Route>
   </Router>,
   document.getElementById('app')
 );
+
+//
+// import store from './store';
+// import {setLyrics} from './action-creators/lyrics';
+//
+// const unsubscribe = store.subscribe(function () {
+//     console.log('----------------');
+//     console.log('State changed!!', store.getState());
+// });
+//
+//
+// //cannot "resubscribe" after unsubscribing
+// store.dispatch(setLyrics('I can feel it coming in the air tonight ... hold on ...'));
+// store.dispatch(setLyrics('Never gonna give you up, never gonna let you down'));
+// // console.log(store);
+// // unsubscribe();
+//
+// console.log(store.getState());
+// console.log(store.getState());
+// unsubscribe();
+//
+// // console.log(store);
+//
+// store.dispatch(setLyrics('Hello, darkness, my old friend.'));
+// store.dispatch(setLyrics('hello world'))
+// console.log(store.getState());
+//
+// // unsubscribe();
